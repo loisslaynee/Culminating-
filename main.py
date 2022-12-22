a03ff01b7c2eaeb370e4fc2a046244b2c09789d5
Lois Layne - <120601281+loisslaynee@users.noreply.github.com>
2022-12-22 17:42:51 +0000

    cause

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
+
+# required imports
 import pygame,sys,random
 from pygame.locals import *
-from colored import fg
 from button import Button
 from tabulate import tabulate
-
-color_1 = fg('red')
-color_2 = fg('blue')
-color_3 = fg('green')
 
 pygame.init()
 
@@ -14,27 +11,31 @@ def tictactoe():
   #Open Pygame window
   screen = pygame.display.set_mode((640, 480)) # add RESIZABLE or FULLSCREEN
   
-  #Title: Tic Tac Toe
+  # set title as Tic Tac Toe
   pygame.display.set_caption("tic tac toe")
-  color=pygame.color.THECOLORS["black"] #color of the background
+  color = pygame.color.THECOLORS["black"] # background colour
 
-  #sets up the table for the
-  tab= pygame.image.load("resources/morp_tab.png").convert()
+  # sets up the table of the game
+  tab = pygame.image.load("resources/ttt.png").convert() #the grid
   tab.set_colorkey((0,0,0))
-  tab_pos =(170,90)
-  
-  cross= pygame.image.load("resources/cross.png").convert()
-  cross.set_colorkey((0,0,0))
-  cross_list=[]
-  
-  circle= pygame.image.load("resources/circle.png").convert()
-  circle.set_colorkey((0,0,0))
-  circle_list=[]
+  tab_pos = (170,90)
+
+  # aspects of the game (yellow emoji)
+  yellow = pygame.image.load("resources/yellow.png").convert()
+  yellow.set_colorkey((0,0,0))
+  yellow_list = []
+
+  # aspects of the game (purple emoji)
+  purple = pygame.image.load("resources/purple.png").convert()
+  purple.set_colorkey((0,0,0))
+  purple_list=[]
   inspect_line=[]
   cont=0
   selected=0
+
+  #stores the number of scores and levels
   cpu_level=2
-  win=0
+  win = 0
   player=0
   cpu=0
   lose=0
@@ -43,71 +44,62 @@ def tictactoe():
   r=0
   time=0
   turn="player"
-  font=pygame.font.SysFont('Britannic Bold', 30)       
   
+  # text font in the game
+  font=pygame.font.SysFont('dejavuserif', 20)       
+
+  #location of the tabs for the emojies
   tab_case=[(170,90),(270,90),(370,90),
            (170,190),(270,190),(370,190),
            (170,290),(270,290),(370,290)]
   
-  
-  t=tab_case
-  win_line=[ [t[0],t[1],t[2]], [t[3],t[4],t[5]],[t[6],t[7],t[8]],
+  #sets 't' as the tab case
+  t = tab_case
+  win_line = [ [t[0],t[1],t[2]], [t[3],t[4],t[5]],[t[6],t[7],t[8]],
              [t[0],t[3],t[6]], [t[1],t[4],t[7]], [t[2],t[5],t[8]],
-             [t[0],t[4],t[8]], [t[6],t[4],t[2]] ]
-             
-  
+             [t[0],t[4],t[8]], [t[6],t[4],t[2]] ] # all the possible winning tabs; 3 in a row
   pygame.key.set_repeat(400, 30)
-  
+
+  # writing the module
   while True:
-    
     #loop speed limitation
-    #30 frames per second is enough
-    pygame.time.Clock().tick(3)
+    pygame.time.Clock().tick(3) #30 frames per second is enough
    
     for event in pygame.event.get():
-      if event.type==pygame.QUIT:    #wait for events
+      if event.type == pygame.QUIT:    #wait for events
         pygame.quit()
-        sys.exit()
-        
-      #keyboard commands
-      if event.type == KEYDOWN:
-         if event.key == K_UP:
-            if cpu_level<=1:
-               cpu_level+=1
-         if event.key == K_DOWN:
-            if cpu_level>=1:
-               cpu_level-=1
+        sys.exit() 
               
       #mouse commands
       if event.type == MOUSEBUTTONDOWN:
          if win or lose or draw:
-            cross_list=[];circle_list=[];win=0;lose=0;draw=0;screen.fill(color);turn="player"
+            yellow_list = []; purple_list =[]; win=0; lose=0; draw=0; screen.fill(color);turn = "player"
             screen.blit(tab,tab_pos);pygame.display.flip()
          if event.button == 1:
-            if turn=="player":
+            if turn == "player":
                for case in tab_case:
                  if event.pos[0]>case[0]and event.pos[0]<case[0]+100 \
                  and event.pos[1]>case[1]and event.pos[1]<case[1]+100 :
-                     if not case in cross_list and not case in circle_list:
-                        selected=1
-                        cross_list.append(case)
-                        screen.blit(cross,case)
+                     if not case in yellow_list and not case in purple_list:
+                        selected = 1
+                        yellow_list.append(case)
+                        screen.blit(yellow, case) #displays the yellow emoji
                         
                for line in win_line:
-                   cont=0
+                   cont = 0
                    for case in line:
-                       for cross1 in cross_list:
-                           if cross1==case:cont+=1
-                   if cont==3 and not lose:
-                      win=1
+                       for yellow1 in yellow_list:
+                           if yellow1 == case:cont += 1
+                   if cont == 3 and not lose:
+                      win = 1
                if win:
-                  player+=1
+                  player += 1
                if selected:
-                  cont=0
-                  selected=0
+                  cont = 0
+                  selected = 0
                   turn="cpu"
                
-    if turn=="cpu" and not win:
+    if turn == "cpu" and not win:
       time+=1
       if time==3:
        time=0
@@ -117,124 +109,124 @@ def tictactoe():
               if not selected:
                  cont=0
                  for case in line:
-                    for circle1 in circle_list:
-                     if circle1==case:cont+=1
+                    for purple1 in purple_list:
+                     if purple1==case:cont+=1
                      
-              if cont==2 and not selected:
+              if cont == 2 and not selected:
                  for case in line:
-                     for circle1 in  circle_list:
-                         if case_inspect!=circle1:
-                            if not case in circle_list and not case in cross_list:
+                     for purple1 in  purple_list:
+                         if case_inspect != purple1:
+                            if not case in purple_list and not case in yellow_list:
                              selected=1
-                             circle_list.append(case)
-                             screen.blit(circle,case)
+                             purple_list.append(case)
+                             screen.blit(purple, case) #displays the purple emoji
         
        if not selected and cpu_level==2:
          
              for line in win_line:
               if not selected:
-                 cont=0
+                 cont = 0
                  for case in line:
-                    for cross1 in cross_list:
-                     if cross1==case:cont+=1
+                    for yellow1 in yellow_list:
+                     if yellow1 == case:cont += 1
                        
               if cont==2 and not selected:
                  for case in line:
-                     for cross1 in  cross_list:
-                         if case!=cross1:
-                            if not case in circle_list and not case in cross_list:
-                             selected=1
-                             circle_list.append(case)
-                             screen.blit(circle,case)
+                     for yellow1 in yellow_list:
+                         if case != yellow1:
+                            if not case in purple_list and not case in yellow_list:
+                             selected = 1
+                             purple_list.append(case)
+                             screen.blit(purple, case)
   
                              
        if not selected and cpu_level>=1:
          
              for line in win_line:
               if not selected:
-                 cont=0
+                 cont = 0
                  for case in line:
-                    for circle1 in circle_list:
-                     if circle1==case:cont+=1
-                    for cross1 in cross_list:
-                     if cross1==case:cont=0
+                    for purple1 in purple_list:
+                     if purple1 == case:cont += 1
+                    for yellow1 in yellow_list:
+                     if yellow1 == case:cont = 0
             
-              if cont==1 and not selected:
+              if cont == 1 and not selected:
                 for index in range(len(line)):
-                    for circle1 in  circle_list:
+                    for purple1 in  purple_list:
                       if not selected:
-                        if line[index]==circle1 and line[index]!=line[len(line)-1]:
-                          if not line[index+1] in circle_list and not line[index+1] in cross_list:
-                            selected=1
-                            circle_list.append(line[index+1])
-                            screen.blit(circle,line[index+1])
-                        elif line[index]==circle1 and line[index]==line[len(line)-1]:
-                          if not line[index-1] in circle_list and not line[index-1] in cross_list:
-                            selected=1
-                            circle_list.append(line[index-1])
-                            screen.blit(circle,line[index-1])
+                        if line[index] == purple1 and line[index] != line[len(line)-1]:
+                          if not line[index+1] in yellow_list and not line[index+1] in yellow_list:
+                            selected = 1
+                            purple_list.append(line[index+1])
+                            screen.blit(purple,line[index+1])
+                        elif line[index] == purple1 and line[index] == line[len(line)-1]:
+                          if not line[index-1] in purple_list and not line[index-1] in yellow_list:
+                            selected = 1
+                            purple_list.append(line[index-1])
+                            screen.blit(purple,line[index-1])
   
                             
        if not selected and cpu_level>=0:
          
          for case in tab_case:
            if not selected:
-             cont=0
+             cont = 0
              r=random.randint(0,8)
-             for cross1 in cross_list:
-                 if tab_case[r]==cross1:cont+=1
-             for circle1 in circle_list:
-                 if tab_case==case:cont+=1
+             for yellow1 in yellow_list:
+                 if tab_case[r] == yellow1:cont += 1
+             for purple1 in purple_list:
+                 if tab_case == case:cont += 1
              if not cont:
-                if not tab_case[r] in circle_list and not tab_case[r] in cross_list:
-                   selected=1
-                   circle_list.append(tab_case[r])
-                   screen.blit(circle,tab_case[r])
+                if not tab_case[r] in purple_list and not tab_case[r] in yellow_list:
+                   selected = 1
+                   purple_list.append(tab_case[r])
+                   screen.blit(purple,tab_case[r])
             
        if not selected and cpu_level>=0:
          
          for case in tab_case:
            if not selected:
-             cont=0
-             for cross1 in cross_list:
-                 if cross1==case:cont+=1
-             for circle1 in circle_list:
-                 if circle1==case:cont+=1
+             cont = 0
+             for yellow1 in yellow_list:
+                 if yellow1 == case:cont += 1
+             for purple1 in purple_list:
+                 if purple1 == case:cont += 1
              if not cont:
-                if not case in circle_list and not case in cross_list:
-                   selected=1
-                   circle_list.append(case)
-                   screen.blit(circle,case)
+                if not case in purple_list and not case in yellow_list:
+                   selected = 1
+                   purple_list.append(case)
+                   screen.blit(purple,case)
                    
-       for circle1 in circle_list:
+       for purple1 in purple_list:
            for line in win_line:
                cont=0
                for case in line:
-                   if circle1==case:
+                   if purple1==case:
                       inspect_line=line
                       for case_inspect in inspect_line:
-                           for circle2 in  circle_list:
-                               if case_inspect==circle2:
-                                  cont+=1         
-               if cont==3:
+                           for purple2 in  purple_list:
+                               if case_inspect == purple2:
+                                  cont += 1         
+               if cont == 3:
                   lose=1
        if lose:
-          cpu+=1
-       cont=0
-       cont2=0
-       selected=0
-       turn="player"
+          cpu += 1
+       cont = 0
+       cont2 = 0
+       selected = 0
+       turn = "player"
        
     if not win and not lose and not draw:
       for case in tab_case:
-          for cross1 in cross_list:
-              if cross1==case:cont+=1
-          for circle1 in circle_list:
-              if circle1==case:cont+=1
-          if cont==9:
-             draw=1
-             draw2+=1
-      cont=0
+          for yellow1 in yellow_list:
+              if yellow1 == case:cont += 1
+          for purple1 in purple_list:
+              if purple1 == case:cont += 1
+          if cont == 9:
+             draw = 1
+             draw2 += 1
+      cont = 0
       
     screen.fill(color)
     
@@ -245,16 +237,16 @@ def tictactoe():
     elif draw:
        text=font.render(("IT'S A TIE!"), True, (0,0,250));screen.blit(text,(285,400))
        
-    text=font.render(("cpu level="+str(cpu_level)), True, (250,250,250));screen.blit(text,(480,0))
-    text=font.render(("CPU="+str(cpu)), True, (250,0,0));screen.blit(text,(180,0))
-    text=font.render(("You="+str(player)), True, (0,250,0));screen.blit(text,(0,0))
-    text=font.render(("Draw="+str(draw2)), True, (0,0,250));screen.blit(text,(340,0))
+    text=font.render(("cpu level = "+str(cpu_level)), True, (250,250,250));screen.blit(text,(480,0))
+    text=font.render(("cpu = "+str(cpu)), True, (250,0,0));screen.blit(text,(180,0))
+    text=font.render(("You = "+str(player)), True, (0,250,0));screen.blit(text,(0,0))
+    text=font.render(("Draw = "+str(draw2)), True, (0,0,250));screen.blit(text,(340,0))
   
     screen.blit(tab,tab_pos)
-    for cross_pos in cross_list:
-        screen.blit(cross,cross_pos)
-    for circle_pos in circle_list:
-        screen.blit(circle,circle_pos)
+    for yellow_pos in yellow_list:
+        screen.blit(yellow, yellow_pos)
+    for purple_pos in purple_list:
+        screen.blit(purple, purple_pos)
   
     pygame.display.flip()
 
@@ -296,6 +288,7 @@ def ty_image():
   push_image("ty","ty.png")
   pygame.display.update()
 
+#function to show instructions when the user clicks instructions on the menu
 def instruction():
   push_image("instruction","instruction.png")
   pygame.display.update()
@@ -330,12 +323,12 @@ def main_menu():
                 if playbutton.checkForInput(MENU_MOUSE_POS):
                     tictactoe() # calls and plays game
                 if insbutton.checkForInput(MENU_MOUSE_POS):
-                    instruction()
-                pygame.time.wait(25000)
+                    instruction() # displays the image with instructions
+                    pygame.time.wait(25000) # displays the image in the given time
 
                 if quitbutton.checkForInput(MENU_MOUSE_POS):
                     ty_image() # show thank you image
-                    pygame.time.wait(5000) # displays the image in a given time
+                    pygame.time.wait(5000) # displays the image in the given time
                     pygame.quit() # exit
                     sys.exit()
 
